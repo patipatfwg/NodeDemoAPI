df1cb1bbe468c6a35ab7c256ae6e21c899373b76
patipatfwg <patipat.fwg@gmail.com>
2019-08-26 00:25:38 +0700

    --- Auto-Git Commit ---

--- server.js
+++ server.js
@@ -60,9 +60,11 @@ app.get('/api/receiptUpload/',(req,res)=> {
 app.get('/api/receiptUpload/ranking/:type',(req,res)=> {
     var type =  req.params.type;
     if(type === "2"){
-        var sql = "SELECT employee.sport_team FROM employee INNER JOIN save_the_earth ON save_the_earth.employee_id = employee.employee_id order by COUNT(save_the_earth.employee_id) DESC";
+        var sql = "SELECT employee.color FROM save_the_earth INNER JOIN employee ON save_the_earth.employee_id = employee.employee_id GROUP BY employee.color order by  save_the_earth.employee_id DESC Limit 4";
+        var msg = 'Color Ranking';
     }else if(type === "1"){
         var sql = "SELECT employee.team FROM save_the_earth INNER JOIN employee ON save_the_earth.employee_id = employee.employee_id GROUP BY employee.team order by  save_the_earth.employee_id DESC Limit 10";
+        var msg = 'Team Ranking';
     }
     
     getConn('hr').query(sql,(err,rows,results) => { 
