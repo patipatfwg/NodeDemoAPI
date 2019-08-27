5c7cb429a64bf6a23f426d781fa3c140c32e7be5
patipatfwg <patipat.fwg@gmail.com>
2019-08-27 14:08:46 +0700

    up

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const bodyParser = require('body-parser');
 const moment = require('moment');
 var momentz = require('moment-timezone');
 
-// const base64ToImage = require('base64-to-image');
-var base64Img = require('base64-img');
+const image2base64 = require('image-to-base64');
 var multer  = require('multer');
 // var fs = require('fs');
 // const ejs = require('ejs');
@@ -46,13 +45,16 @@ function getConn(aa){
     }
 }
 ///////////////////////////////////////////////////////////////////////
-///////////////////////// Set ////////////////////////////////////////////
+
+///////////////////////// Set /////////////////////////////////////////
+var fileImg = 'save_the_earth'+'-'+Date.now()+'.png';
 var storage = multer.diskStorage({
+
     destination: function (req, file, cb) {
       cb(null, 'uploads')
     },
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now()+'.jpg')
+      cb(null, fileImg)
     }
   })
    
@@ -96,49 +98,69 @@ app.get('/api/receiptUpload/ranking/:type',(req,res)=> {
     })
 })
 
-app.post('/api/receiptUpload',function (req,res){  
-    // const user_id =  req.body.user_id;
-    // const section_id =  req.body.section_id;
-    // const app_title = req.body.app_title;
-    // const download_at = moment().tz('Asia/Bangkok').format("YYYY-MM-DD HH:mm:ss");
+// app.post('/api/receiptUpload_',function (req,res){  
+//     // const user_id =  req.body.user_id;
+//     // const section_id =  req.body.section_id;
+//     // const app_title = req.body.app_title;
+//     // const download_at = moment().tz('Asia/Bangkok').format("YYYY-MM-DD HH:mm:ss");
 
-    const receipt_date = req.body.receipt_date;
-    const receipt_no = req.body.receipt_no;  
-    const images =  req.body.images;  
-    const employee_id =  req.body.employee_id;
+//     const receipt_date = req.body.receipt_date;
+//     const receipt_no = req.body.receipt_no;  
+//     const images =  req.body.images;  
+//     const employee_id =  req.body.employee_id;
 
-    const sql = "INSERT INTO save_the_earth (receipt_date,receipt_no,images,employee_id) VALUES (?,?,?,?)";
-    getConn('hr').query(sql, [receipt_date,receipt_no,images,employee_id], function (err, rows, fields) {
-        if(!err){
+//     const sql = "INSERT INTO save_the_earth (receipt_date,receipt_no,images,employee_id) VALUES (?,?,?,?)";
+//     getConn('hr').query(sql, [receipt_date,receipt_no,images,employee_id], function (err, rows, fields) {
+//         if(!err){
 
-            // res.json({ "HEAD": fields ,"BODY": rows ,"MESSAGE": "Add Success" })  
+//             // res.json({ "HEAD": fields ,"BODY": rows ,"MESSAGE": "Add Success" })  
 
-            getConn('hr').query('SELECT * FROM save_the_earth',(err,rows,results) => { 
-                if(!err){
-                    res.json({ "HEAD": rows.length ,"BODY": rows ,"MESSAGE": "Add Success" })   
-                }else{
-                    res.json(err)
-                    console.log(err);
-                }   
-            })
+//             getConn('hr').query('SELECT * FROM save_the_earth',(err,rows,results) => { 
+//                 if(!err){
+//                     res.json({ "HEAD": rows.length ,"BODY": rows ,"MESSAGE": "Add Success" })   
+//                 }else{
+//                     res.json(err)
+//                     console.log(err);
+//                 }   
+//             })
 
-        }else{
-            res.json(err)
-        }       
-    });  
-})
+//         }else{
+//             res.json(err)
+//         }       
+//     });  
+// })
 
-app.post('/api/uploadfile', upload.single('myFile'), (req, res, next) => {
+app.post('/api/receiptUpload', upload.single('myFile'), (req, res, next) => {
     const file = req.file
+    const images = 'uploads/'+fileImg
     if (!file) {
-      const error = new Error('Please upload a file')
+      const error = new Error('Please upload a images')
       error.httpStatusCode = 400
       return next(error)
+    }else{
+        var receipt_date = req.body.receipt_date;
+        var receipt_no = req.body.receipt_no;
+        var employee_id = req.body.employee_id;
+        const sql = "INSERT INTO save_the_earth (receipt_date,receipt_no,images,employee_id) VALUES (?,?,?,?)";
+        getConn('hr').query(sql, [receipt_date,receipt_no,images,employee_id], function (err, rows, fields) {
+            if(!err){
+    
+                // res.json({ "HEAD": fields ,"BODY": rows ,"MESSAGE": "Add Success" })  
+    
+                getConn('hr').query('SELECT * FROM save_the_earth WHERE'+employee_id,(err,rows,results) => { 
+                    if(!err){
+                        res.json({ "HEAD": rows.length ,"BODY": rows ,"MESSAGE": "Add Success" })   
+                    }else{
+                        res.json({ "HEAD": "","BODY": "" ,"MESSAGE": err })
+                        console.log(err);
+                    }   
+                })
+    
+            }else{
+                res.json(err)
+            }       
+        }); 
     }
-    //   res.send(file)
-
-    var based64Img = base64Img.base64Sync(file);
-    console.log(based64Img)
     
 })
 /////////////////////////////////////////////////////////////////////
